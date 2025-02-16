4ca34d3fbafc8334d686b144509844c414a153d9
Elgedawy31 <mohamedelgedawy40@gmail.com>
2025-02-16 14:57:58 +0200

    save changes

--- src/components/empolyees/Tabs/DetailsTab.jsx
+++ src/components/empolyees/Tabs/DetailsTab.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import CardContainer from '../../CardContainer'
 import Info from '../detailsTab/Info'
 import MonthlyDashboard from '../detailsTab/MonthlyDashboard'
+import PersonalInformation from '../detailsTab/PersonalInformation'
 
 function DetailsTab() {
   return (
     <CardContainer className='p-4 space-y-6'>
         <Info />
         <MonthlyDashboard />
+        <PersonalInformation />
     </CardContainer>
   )
 }

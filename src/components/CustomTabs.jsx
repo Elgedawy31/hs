59a87eb5cb0b6630c2e60b38a4c41ba64173ac0a
Elgedawy31 <mohamedelgedawy40@gmail.com>
2025-02-16 11:54:46 +0200

    updates  and add custom tabs

---
+++ src/components/CustomTabs.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function CustomTabs({ tabs, activeTab, onChange, children }) {
+  return (
+    <div className="w-full rounded-lg">
+      <div className="">
+        <div className="flex relative">
+          {tabs.map((tab) => (
+            <button
+              key={tab.id}
+              onClick={() => onChange(tab.id)}
+              className={`relative px-6 py-4 text-sm font-medium transition-all duration-300 ease-in-out
+                ${
+                  activeTab === tab.id
+                    ? "text-primary"
+                    : "text-text hover:text-hoverText"
+                }
+              `}
+            >
+              {tab.label}
+              <div 
+                className={`absolute bottom-0 left-0 w-full h-0.5 transform transition-all duration-300 ease-in-out
+                  ${activeTab === tab.id 
+                    ? "bg-primary scale-x-100 opacity-100" 
+                    : "scale-x-0 opacity-0"
+                  }
+                `} 
+              />
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className="animate-fadeIn">
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default CustomTabs
--- src/pages/Empolyees/EmployeeDetails.jsx
+++ src/pages/Empolyees/EmployeeDetails.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CustomTabs from '../../components/CustomTabs'
+import CardContainer from '../../components/CardContainer';
 
 function EmployeeDetails() {
+  const [activeTab, setActiveTab] = useState("details");
+
+  const tabs = [
+    { id: "details", label: "Details" },
+    { id: "daily", label: "Daily Activity" },
+    { id: "leaves", label: "Leaves" },
+  ];
+
   return (
-    <div>
-      EmployeeDetails
+    <div className="w-full">
+      <CustomTabs
+        tabs={tabs}
+        activeTab={activeTab}
+        onChange={setActiveTab}
+      >
+      </CustomTabs>
+
+      
+     
     </div>
   )
 }

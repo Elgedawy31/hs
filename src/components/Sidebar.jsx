f6aafe9b3107b7e8424676ee50341ccb44965a3b
Elgedawy31 <mohamedelgedawy40@gmail.com>
2025-02-23 10:47:07 +0200

    udpates on sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,21 +14,24 @@ export default function Sidebar({ sidebarOpen, setSidebarOpen }) {
   const {user:{role}} = useAuth()
   const MenuItem = ({ icon: Icon, label, path }) => (
     <button
-      onClick={() => navigate(`${role ==='user' ? '/dashboard' : '/'}/${path}`)}
+      onClick={() => navigate(`${role ==='user' ? '/dashboard' : ''}/${path}`)}
       className={`w-full flex items-center relative group transition-all duration-300 ease-in-out text-sm sm:text-base`}
     >
       <div
-        className={`absolute left-0 w-[2px] sm:w-[3px] h-full transform transition-all duration-300 ease-in-out 
-          ${location?.pathname.includes(path) ? 'bg-primary scale-y-100' : 'bg-transparent scale-y-0 group-hover:scale-y-100 group-hover:bg-primary/50'}`}
+      className={`absolute left-0 w-[2px] sm:w-[3px] h-full transform transition-all duration-300 ease-in-out 
+          ${path === "" ? ((location?.pathname === "/dashboard" || location.pathname ==='/dashboard/') ? 'bg-primary scale-y-100' : 'bg-transparent scale-y-0 group-hover:scale-y-100 group-hover:bg-primary/50') 
+          : location?.pathname.includes(path) ? 'bg-primary scale-y-100' : 'bg-transparent scale-y-0 group-hover:scale-y-100 group-hover:bg-primary/50'}`}
       />
       <div
         className={`flex items-center w-full px-3 sm:px-4 py-2 transform transition-all duration-300 ease-in-out
-          ${location?.pathname.includes(path) ? 'bg-secondPrimaryColor translate-x-1' : 'bg-transparent hover:translate-x-1 hover:bg-secondPrimaryColor/50'}
+          ${path === "" ? ((location?.pathname === "/dashboard" || location.pathname ==='/dashboard/') ? 'bg-secondPrimaryColor translate-x-1' : 'bg-transparent hover:translate-x-1 hover:bg-secondPrimaryColor/50')
+          : location?.pathname.includes(path) ? 'bg-secondPrimaryColor translate-x-1' : 'bg-transparent hover:translate-x-1 hover:bg-secondPrimaryColor/50'}
           text-text hover:text-hoverText`}
       >
         <Icon
           className={`w-5 h-5 sm:w-6 sm:h-6 transition-transform duration-300 ease-in-out group-hover:scale-110 
-            ${location?.pathname.includes(path) ? 'text-primary' : 'text-text'}`}
+            ${path === "" ? ((location?.pathname === "/dashboard" || location.pathname ==='/dashboard/') ? 'text-primary' : 'text-text')
+            : location?.pathname.includes(path) ? 'text-primary' : 'text-text'}`}
         />
         <span className="ml-2 sm:ml-3 flex-1 text-left font-medium transition-all duration-300">{label}</span>
       </div>

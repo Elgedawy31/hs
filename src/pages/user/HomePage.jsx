d01ea9a683606354a1570a12676c71220b8281a4
Elgedawy31 <mohamedelgedawy40@gmail.com>
2025-02-24 20:03:00 +0200

    feat: add HomeCard component and update HomePage layout with new card design

--- src/pages/user/HomePage.jsx
+++ src/pages/user/HomePage.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react'
 import { useAuth } from '../../contexts/AuthContext'
 import { useNavigate } from 'react-router-dom'
+import HomeCard from '../../components/user/bonus/HomeCard'
+import CardContainer from '../../components/CardContainer'
+import { CircleCheckBig, ClockAlert, StepForward } from 'lucide-react'
 
 function HomePage() {
     const {user} = useAuth()
@@ -11,13 +14,13 @@ function HomePage() {
         }
     } , [user])
   return (
-    <div>
-      <div className="grid grid-cols-3 gap-4">
-        <div>osama</div>
-        <div>osama</div>
-        <div>osama</div>
+    <CardContainer>
+      <div className="grid grid-cols-3 gap-8">
+        <HomeCard Icon={StepForward} title='Tracked Time' description='167 h 19 m' />
+        <HomeCard Icon={CircleCheckBig} title='Producitity' description='167 h 19 m' />
+        <HomeCard Icon={ClockAlert} title='Overtime' description='16 h' />
       </div>
-    </div>
+    </CardContainer>
   )
 }
 

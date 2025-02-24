5bd9af305c96b941e588b19a07591bc9f7c8a97f
Elgedawy31 <mohamedelgedawy40@gmail.com>
2025-02-24 20:21:33 +0200

    feat: enhance HomePage layout with user greeting and improved spacing

--- src/pages/user/HomePage.jsx
+++ src/pages/user/HomePage.jsx
@@ -6,15 +6,23 @@ import CardContainer from '../../components/CardContainer'
 import { CircleCheckBig, ClockAlert, StepForward } from 'lucide-react'
 
 function HomePage() {
-    const {user} = useAuth()
-    const navigate = useNavigate()
-    useEffect(() => {
-        if(user?.role ==='admin'){
-            navigate('/dashboard')
-        }
-    } , [user])
+  const { user } = useAuth()
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (user?.role === 'admin') {
+      navigate('/dashboard')
+    }
+  }, [user])
   return (
-    <CardContainer className='p-6'>
+    <CardContainer className='p-6 space-y-10'>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <img src="https://i.pravatar.cc/150?u=a04258114e29026708c"
+            alt="" className='w-[42px] h-[42px] rounded-full ' />
+          <h1 className="text-xl font-medium">Good Morning <span className="text-xl font-meduim text-placeholderText">Gedawy !</span> </h1>
+
+        </div>
+      </div>
       <div className="grid grid-cols-3 gap-8">
         <HomeCard Icon={StepForward} title='Tracked Time' description='167 h 19 m' />
         <HomeCard Icon={CircleCheckBig} title='Producitity' description='167 h 19 m' />

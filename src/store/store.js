34ec4e390df252ba322507011032d2636a3b587d
Elgedawy31 <mohamedelgedawy40@gmail.com>
2025-02-27 21:51:44 +0200

    feat: add users slice with CRUD operations and integrate into store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import emailReducer from './reducers/email';
 import notificationReducer from './reducers/notification';
+import usersReducer from './reducers/users';
 
 export const store = configureStore({
     reducer: {
         box: emailReducer,
-        notification: notificationReducer
+        notification: notificationReducer,
+        users: usersReducer
     }
 });

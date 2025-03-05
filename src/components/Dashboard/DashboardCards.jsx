0d0d27b54744c8252613cd59da92c64d40d53edb
Elgedawy31 <mohamedelgedawy40@gmail.com>
2025-03-05 14:21:31 +0200

    refactor: adjust grid layout in DashboardCards for improved responsiveness

--- src/components/Dashboard/DashboardCards.jsx
+++ src/components/Dashboard/DashboardCards.jsx
@@ -62,7 +62,7 @@ function DashboardCards() {
   return (
     <div className="p-4 space-y-6">
       
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ">
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-4 ">
         <UniCard 
           title="Total Employees" 
           value="50"

f13dd3a64702ff48b17c7c132c4e2a87218262ac
Elgedawy31 <mohamedelgedawy40@gmail.com>
2025-03-06 16:50:48 +0200

    refactor: add OurProducts component to HomePage for enhanced product presentation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/home/Hero'
 import CanDo from '../components/home/CanDo'
+import OurProducts from '../components/home/OurProducts'
 
 function HomePage() {
   return (
     <div className='container mx-auto'>
     <Hero />
     <CanDo />
+    <OurProducts />
     </div>
   )
 }
